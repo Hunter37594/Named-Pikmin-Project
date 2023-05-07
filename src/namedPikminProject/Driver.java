0e7982707ef55610da8ca011432317a654be781d
Hunter37594 <117316321+Hunter37594@users.noreply.github.com>
2023-05-07 00:32:47 -0500

    Implement basic prototype and add initial viewer list

--- src/namedPikminProject/Driver.java
+++ src/namedPikminProject/Driver.java
@@ -17,55 +17,111 @@ import java.awt.event.*;
  * - 
  */
 
-public class Driver implements ActionListener {
+public class Driver {
 	
 	// Declare class data
 	public static Image raccoon;
 	private static JFrame frame;
 	private static JPanel panel;
-	private static JButton promote;
-	private static JComboBox<Integer> time;
+	private static Pikmin currPik;
+	private static PikColor currColor;
 	
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
-    	//raccoon = ImageIO.read(new File(("raccoon.png")));
     	
-    	Pikmin.add(new Pikmin("Steve", PikColor.RED, Bloom.LEAF));
+    	Pikmin.readFile("pikmanager.csv");
     	
     	// Set up frame, include your name in the title
     	frame = new JFrame("Pikmanager");
     	frame.setSize(1920, 1080);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	frame.setVisible(true);
-    	frame.setLayout(new BorderLayout());
+    	frame.setLayout(new GridBagLayout());
         
-        // Set up Panel for input selections
-        panel = new JPanel();
     	
-        // Promote Button
-        promote = new JButton("Promote");
-        promote.addActionListener(new Driver());
-    	
-        // ComboBox to pick animation time
-        time = new JComboBox<Integer>();
-        time.addItem(15);
-        time.addItem(30);
-        time.addItem(60);
-        time.addItem(90);
-    	
-        // Add all to top panel
-        panel.add(promote);
-        panel.add(time);
+        // Drop-down to select pikmin
+        JComboBox<String> pikCombo = new JComboBox<String>();
+        JLabel pikLabel = new JLabel();
+        pikCombo.addItemListener(new ItemListener() {
+        	public void itemStateChanged(ItemEvent e) {
+        		currPik = Pikmin.get((String) pikCombo.getSelectedItem());
+        		pikLabel.setText(currPik.getColor().toString() + " " + currPik.getBloom().toString());
+        	}
+        });
+        for (Pikmin p : Pikmin.getPikminList()) {
+        	pikCombo.addItem(p.getName());
+        }
         
-        frame.add(panel, BorderLayout.PAGE_START);
+        // Drop-down to select destination color
+        // TODO: Display img instead of text label
+        JComboBox<String> candyCombo = new JComboBox<String>();
+        candyCombo.addItemListener(new ItemListener() {
+        	public void itemStateChanged(ItemEvent e) {
+        		currColor = PikColor.valueOf((String) candyCombo.getSelectedItem());
+        	}
+        });
+        candyCombo.addItem("RED");
+        candyCombo.addItem("YELLOW");
+        candyCombo.addItem("BLUE");
         
-    }
-    
-    // Animate the trip based on selections from the GUI components
-    public void actionPerformed(ActionEvent event) {
-        Pikmin.get("Steve").promote();
-        Pikmin.get("Steve").candypop(PikColor.BLUE);
-        System.out.println(Pikmin.get("Steve"));
+        // Promotes selected pikmin by one tier
+        JButton promote = new JButton(new AbstractAction("Promote") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+            	Pikmin p = Pikmin.get((String) pikCombo.getSelectedItem());
+            	p.promote();
+            	System.out.println(p);
+            }
+        });
+        // Demotes selected pikmin by one tier
+        JButton demote = new JButton(new AbstractAction("Demote") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+            	Pikmin p = Pikmin.get((String) pikCombo.getSelectedItem());
+            	p.demote();
+            	System.out.println(p);
+            }
+        });
+        
+        JButton candypop = new JButton(new AbstractAction("Candypop") {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+                currPik.candypop(currColor);
+                System.out.println(currPik);
+            }
+        });
+        
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = c.weighty = 1.0;
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        frame.add(pikCombo, c);
+        
+        c.gridx = 1;
+        c.gridy = 0;
+        frame.add(pikLabel, c);
+        
+        c.gridx = 1;
+        c.gridy = 2;
+        frame.add(candyCombo, c);
+        
+        c.gridx = 0;
+        c.gridy = 1;
+        frame.add(promote, c);
+        
+        c.gridx = 1;
+        c.gridy = 1;
+        frame.add(demote, c);
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        frame.add(candypop, c);
+        
+        frame.setIconImage(ImageIO.read(new File("louie.jpg")));
+        
+        System.out.println(Pikmin.getPikminList());
     }
     
 }
--- src/namedPikminProject/Pikmin.java
+++ src/namedPikminProject/Pikmin.java
@@ -1,6 +1,10 @@
 package namedPikminProject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pikmin {
 	
@@ -30,7 +34,7 @@ public class Pikmin {
 	public static void add(Pikmin p) {
 		pikmin.add(p);
 	}
-	public static void addSquad(Pikmin p) {
+	public static void squadd(Pikmin p) {
 		squad.add(p);
 	}
 	public static void kill (Pikmin p) {
@@ -49,6 +53,17 @@ public class Pikmin {
 		}
 		
 	}
+	public void demote() {
+		
+		if (this.bloom == Bloom.FLOWER) {
+			this.bloom = Bloom.BUD;
+		}
+		
+		else if (this.bloom == Bloom.BUD) {
+			this.bloom = Bloom.LEAF;
+		}
+		
+	}
 	public void candypop(PikColor toColor) {
 		this.color = toColor;
 	}
@@ -56,7 +71,7 @@ public class Pikmin {
 	public static Pikmin get(String name) {
 		
 		for (Pikmin p : pikmin) {
-			if (p.name == name) {
+			if (p.name.equals(name)) {
 				return p;
 			}
 		}
@@ -64,10 +79,46 @@ public class Pikmin {
 		return null;
 		
 	}
+	public static ArrayList<Pikmin> getPikminList() {
+		return pikmin;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public PikColor getColor() {
+		return this.color;
+	}
+	public Bloom getBloom() {
+		return this.bloom;
+	}
+	
+	public static void readFile(String filename) throws FileNotFoundException, IOException {
+
+		// construct a file object for the file with the given name.
+		File file = new File(filename);
+
+		// construct a scanner to read the file.
+		Scanner fileScanner = new Scanner(file);
+
+		// create the Array that will store each lines data so we can grab the time, lat, and lon
+		String[] fileData = null;
+
+		// grab the next line
+		while (fileScanner.hasNextLine()) {
+			String line = fileScanner.nextLine();
+
+			// split each line along the commas
+			fileData = line.split(",");
+			Pikmin p = new Pikmin(fileData[0], PikColor.valueOf(fileData[1]), Bloom.valueOf(fileData[2]));
+		}
+
+		// close scanner
+		fileScanner.close();
+	}
 	
 	@Override
 	public String toString() {
-		return name + ", " + color.toString() + " " + bloom.toString();
+		return name + ": " + color.toString() + " " + bloom.toString();
 	}
 	
 }
